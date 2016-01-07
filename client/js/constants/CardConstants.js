369f5fd5d4bf1e57c1436bb3e3e1837cd7e2ce90
John Bartos <jbartos7@gmail.com>
2016-01-07 16:03:08 -0500

    Reworking api utils

--- client/js/constants/CardConstants.js
+++ client/js/constants/CardConstants.js
@@ -1,5 +1,7 @@
 const actions = {
-  CREATE_CARD: 'CREATE_CARD'
+  CREATE_CARD: 'CREATE_CARD',
+  CARD_GET_STARTED: 'CARD_GET_STARTED',
+  CARD_GET_COMPLETED: 'CARD_GET_COMPLETED'
 };
 
 module.exports = actions;
